e0f8ab2771411b6c5a684bd2381dff6a658462aa
karamond32 <67460945+karamond32@users.noreply.github.com>
2020-06-28 16:26:39 +0300

    kommit3

--- WindowsFormsApp3/WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -101,7 +101,7 @@ namespace WindowsFormsApp3
             if (radioButton2.Checked)
 
             {
-                resp = new TProf("Рыков", "Игнат", 110000, "высшая", "информатика");
+                resp = new TProf("Рыков", "Игнат", 110000, "высшая", "информатика");// ему не повезло(комментарий для 4 ветки)
             }
 
             textBox5.AppendText(resp.info() + "\n");
